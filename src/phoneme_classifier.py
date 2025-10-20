b454df6f5d0dd0c4e9b0a4b4c1c55d6a29671aa9
Jackson Taylor <jacksontay666@gmail.com>
2025-10-20 12:34:53 -0400

    uncommented prior code

--- src/phoneme_classifier.py
+++ src/phoneme_classifier.py
@@ -3,27 +3,25 @@ from transformers import AutoProcessor, AutoModelForCTC # Wav2Vec2PhonemeCTCToke
 import torch
 from torchcodec.decoders import AudioDecoder
 
-decorder = AudioDecoder()
+timit = load_dataset("kylelovesllms/timit_asr_ipa", split="train[:100]")
+timit = timit.train_test_split(test_size=0.2)
+timit = timit.select_columns(["audio", "phonetic_detail"])
 
-# timit = load_dataset("kylelovesllms/timit_asr_ipa", split="train[:100]")
-# timit = timit.train_test_split(test_size=0.2)
-# timit = timit.select_columns(["audio", "phonetic_detail"])
+sampling_rate = 16000 # timit["train"]["audio"].sampling_rate
+audio_file = timit["train"][0]["audio"]
 
-# sampling_rate = 16000 # timit["train"]["audio"].sampling_rate
-# audio_file = timit["train"][0]["audio"]
+processor = AutoProcessor.from_pretrained("facebook/wav2vec2-base")
 
-# processor = AutoProcessor.from_pretrained("facebook/wav2vec2-base")
+inputs = processor(audio_file, sampling_rate=16000, return_tensors="pt")
 
-# inputs = processor(audio_file, sampling_rate=16000, return_tensors="pt")
+model = AutoModelForCTC.from_pretrained("facebook/wav2vec2-base")
 
-# model = AutoModelForCTC.from_pretrained("facebook/wav2vec2-base")
-
-# with torch.no_grad():
-#     logits = model(**inputs).logits
+with torch.no_grad():
+    logits = model(**inputs).logits
     
-# predicted_ids = torch.argmax(logits, dim=-1)
-# transcription = processor.batch_decode(predicted_ids)
-# print(transcription)
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = processor.batch_decode(predicted_ids)
+print(transcription)
 
 # from datasets import load_dataset, Audio
 # from transformers import (
